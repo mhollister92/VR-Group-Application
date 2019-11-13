d7a51e19b7996d52a6cb70b665ef824ea351459a
mhollister92 <mhollister92@gmail.com>
2019-11-13 16:10:18 +0000

    Test Red Sound Wave with Particles

---
+++ VRTK-master/Assets/Scripts/ParticleSystemController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemController : MonoBehaviour
+{
+    public ParticleSystem redParticles;
+    public float particleRadius = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        redParticles = GetComponent<ParticleSystem>();
+        ParticleSystem.ShapeModule shape = redParticles.shape;
+        shape.radius = particleRadius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ParticleSystem.ShapeModule shape = redParticles.shape;
+        shape.radius = particleRadius;
+        particleRadius += 0.01f;
+    }
+}
